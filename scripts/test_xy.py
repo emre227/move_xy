13cd9b3988db5a70c6fff4fde5dc56391c20fabd
emre227 <emre.oencue@hsbi.de>
2023-09-21 17:07:10 +0200

    while loop hinzugefügt

--- scripts/test_xy.py
+++ scripts/test_xy.py
@@ -13,20 +13,20 @@ if __name__ == '__main__':
     rospy.init_node("move_xy")
     pub = rospy.Publisher("/move_base_simple/goal", PoseStamped, queue_size=10)
 
-
-    x,y = (input("Enter X, Y coordinates: ")).split() #liest user input und teilt in x und y auf
-    x = float(x)
-    y = float(y)
-    print(x,y)
+    while not rospy.is_shutdown():
+        x,y = (input("Enter X, Y coordinates: ")).split() #liest user input und teilt in x und y auf
+        x = float(x)
+        y = float(y)
+        print(x,y)
     
-    xy_goal = PoseStamped() 
+        xy_goal = PoseStamped() 
 
-    xy_goal.header.seq = 0
-    xy_goal.header.frame_id = 'map'
-    xy_goal.header.stamp = rospy.Time.now()
-    xy_goal.pose = Pose(Point(x,y,0.0) , Quaternion(0,0,0,1))
+        xy_goal.header.seq = 0
+        xy_goal.header.frame_id = 'map'
+        xy_goal.header.stamp = rospy.Time.now()
+        xy_goal.pose = Pose(Point(x,y,0.0) , Quaternion(0,0,0,1))
     
 
-    pub.publish(xy_goal)
+        pub.publish(xy_goal)
     
-    rospy.spin()
+   
